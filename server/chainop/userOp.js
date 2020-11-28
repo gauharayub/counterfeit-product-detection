5aa7695ee27aaab0c87dc9c995f7cd369e1d9b5f
gauharayub <gauharayub14@gmail.com>
2020-11-28 14:02:12 +0530

    added all remaining methods

--- server/chainop/userOp.js
+++ server/chainop/userOp.js
@@ -1,5 +1,64 @@
 const common = require('./common');
 
-const userOp = {};
+const userOp = {
+  async buyProduct(secretId, privateKey) {
+    try {
+      const signedTransaction = await common.signTransaction(
+        `buyProduct(${secretId})`,
+        privateKey,
+      );
+      const result = await common.sendTransaction(signedTransaction);
+      return result;
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to purchase product');
+    }
+  },
+  async reportSeller(productId, privateKey) {
+    try {
+      const signedTransaction = await common.signTransaction(
+        `registerReport(${productId})`,
+        privateKey,
+      );
+      const result = await common.sendTransaction(signedTransaction);
+      return result;
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to report seller');
+    }
+  },
+  async unblockSeller(sellerAddress, privateKey) {
+    try {
+      const signedTransaction = await common.signTransaction(
+        `unblockSeller(${sellerAddress})`,
+        privateKey,
+      );
+      const result = await common.sendTransaction(signedTransaction);
+      return result;
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to unblock seller');
+    }
+  },
+  async getAllProducts(ownerAddress) {
+    try {
+      const result = await common.callTransaction(
+        `getAllProducts(${ownerAddress})`,
+      );
+      return result;
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to retrieve all products');
+    }
+  },
+  async genKey() {
+    try {
+      return await common.generatePrivateKey();
+    } catch (error) {
+      console.log(error.message);
+      throw new Error('Failed to generate keys');
+    }
+  },
+};
 
 module.exports = userOp;
--- server/routes/userController.js
+++ server/routes/userController.js
@@ -1,67 +1,76 @@
 const manager = require('../manager');
 const userManager = manager.userManager;
-
+const userOp = require('../chainop/userOp');
 const argon2 = require('argon2');
 const jwt = require('jsonwebtoken');
 
 const userController = {
-  login: async function (req, res, next) {
-    try {
-      if (!req.body || !req.body.email || !req.body.password) {
-        throw new Error('Invalid email or password');
-      }
+  // login: async function (req, res, next) {
+  //   try {
+  //     if (!req.body || !req.body.email || !req.body.password) {
+  //       throw new Error('Invalid email or password');
+  //     }
 
-      const email = req.body.email;
-      const password = req.body.password;
+  //     const email = req.body.email;
+  //     const password = req.body.password;
 
-      const rawResponse = await userManager.getPasswordByEmail(email);
-      const response = rawResponse[0];
+  //     const rawResponse = await userManager.getPasswordByEmail(email);
+  //     const response = rawResponse[0];
 
-      if (await argon2.verify(response, password)) {
-        res.cookie('jwt', jwt.sign(email, process.env.JWT_SECRET_KEY));
+  //     if (await argon2.verify(response, password)) {
+  //       res.cookie('jwt', jwt.sign(email, process.env.JWT_SECRET_KEY));
 
-        return res.send('Login successfully');
-      } else {
-        throw new Error('Password does not match');
-      }
-    } catch (error) {
-      return next(error);
-    }
-  },
+  //       return res.send('Login successfully');
+  //     } else {
+  //       throw new Error('Password does not match');
+  //     }
+  //   } catch (error) {
+  //     return next(error);
+  //   }
+  // },
 
-  signup: async function (req, res, next) {
-    try {
-      if (!req.body || !req.body.email || !req.body.password) {
-        throw new Error('Invalid email or password');
-      }
+  // signup: async function (req, res, next) {
+  //   try {
+  //     if (!req.body || !req.body.email || !req.body.password) {
+  //       throw new Error('Invalid email or password');
+  //     }
 
-      const email = req.body.email;
-      const password = req.body.password;
+  //     const email = req.body.email;
+  //     const password = req.body.password;
 
-      await userManager.checkEmailRegistered(email);
+  //     await userManager.checkEmailRegistered(email);
 
-      const hash = await argon2.hash(password);
-      const privateKey = await userManager.generatePrivateKey();
+  //     const hash = await argon2.hash(password);
+  //     const privateKey = await userManager.generatePrivateKey();
 
-      await userManager.storeUser(email, hash, privateKey);
+  //     await userManager.storeUser(email, hash, privateKey);
 
-      res.cookie('jwt', jwt.sign(email, process.env.JWT_SECRET_KEY));
-      res.send('Seller registered successfully');
-    } catch (error) {
-      return next(error);
-    }
-  },
+  //     res.cookie('jwt', jwt.sign(email, process.env.JWT_SECRET_KEY));
+  //     res.send('Seller registered successfully');
+  //   } catch (error) {
+  //     return next(error);
+  //   }
+  // },
 
-  logout: function (req, res, next) {
-    res.clearCookie('jwt');
-    res.redirect('/');
-  },
+  // logout: function (req, res, next) {
+  //   res.clearCookie('jwt');
+  //   res.redirect('/');
+  // },
 
   reportSeller: async function (req, res, next) {
     try {
-      // product ID of the counterfieted product..
-      const productId = req.body.productId;
-      // web3 function call..
+      if (!req.body) {
+        throw new Error('Nothing in request object');
+      }
+
+      const { productId, email } = req.body;
+
+      if (!email || !productId) {
+        throw new Error('Details incomplete');
+      }
+
+      const privateKey = userManager.getPrivateKeyByEmail(email);
+      await userOp.reportSeller(productId, privateKey);
       res.send('Seller reported successfully');
     } catch (error) {
       return next(error);
@@ -70,8 +79,18 @@ const userController = {
 
   unblockSeller: async function (req, res, next) {
     try {
-      const sellerAddress = req.body.sellerAddress;
-      // web3 function call...
+      if (!req.body) {
+        throw new Error('Nothing in request object');
+      }
+
+      const { sellerAddress, email } = req.body;
+
+      if (!email || !sellerAddress) {
+        throw new Error('Details incomplete');
+      }
+      const privateKey = userManager.getPrivateKeyByEmail(email);
+      await userOp.unblockSeller(sellerAddress, privateKey);
+      res.send('Seller unblocked successfully');
     } catch (error) {
       return next(error);
     }
@@ -79,8 +98,17 @@ const userController = {
 
   getAllProducts: async function (req, res, next) {
     try {
-      const products = [];
-      // web3 function call...
+      if (!req.body) {
+        throw new Error('Nothing in request object');
+      }
+      const { ownerAddress, email } = req.body;
+
+      if (!email || !ownerAddress) {
+        throw new Error('Details incomplete');
+      }
+
+      const privateKey = userManager.getPrivateKeyByEmail(email);
+      const products = await userOp.getAllProducts(ownerAddress, privateKey);
       res.send(products);
     } catch (error) {
       return next(error);
@@ -90,9 +118,19 @@ const userController = {
   buyAndVerifyProduct: async function (req, res, next) {
     // call this method after scanning the product...
     try {
+      if (!req.body) {
+        throw new Error('Nothing in request object');
+      }
+
       // secretId of the product extracted from qr code...
-      const secretId = req.body.secretId;
-      // web3 function call..
+      const { secretId, email } = req.body;
+
+      if (!email || !secretId) {
+        throw new Error('Details incomplete');
+      }
+
+      await userOp.buyProduct(secretId);
+      res.send('Purchase and product verification successfull');
     } catch (error) {
       return next(error);
     }
