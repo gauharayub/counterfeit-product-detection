280ee52bc1f258fa7f0c6279b281de67d8385fe5
anas40 <manas1@myamu.ac.in>
2021-01-14 16:15:53 +0530

    add product parameter fix

--- migrations/2_default_migration.js
+++ migrations/2_default_migration.js
@@ -1,5 +1,12 @@
 const Counterfeit = artifacts.require('Counterfeit');
 
 module.exports = async function (deployer) {
-  deployer.deploy(Counterfeit);
+  await deployer.deploy(Counterfeit);
+  let c = await Counterfeit.deployed();
+
+  //arifa Address
+  // await c.transferOwnership('0xcb608e1DcEBA308C96aaE1d7c9800ace8cA559B1')
+
+  //kamran Address
+  // await c.transferOwnership('0x0cf03d9DABa614eF22cB0Aa96730E91521e6e6d6')
 };
