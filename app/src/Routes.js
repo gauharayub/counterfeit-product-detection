90319aeb2956f238759fc0be0f75cfd4af66be16
anas40 <manas1@myamu.ac.in>
2021-01-29 22:09:36 +0530

    common shift

--- app/src/Routes.js
+++ app/src/Routes.js
@@ -1,9 +1,7 @@
-import { useRecoilValue } from 'recoil'
 import { lazy } from 'react';
 import {
-  BrowserRouter as Router, Switch, Route, Redirect
+  BrowserRouter as Router, Switch, Route
 } from "react-router-dom";
-import { login as ll, type as ti } from './store/atoms'
 import Header from './components/header';
 import Footer from './components/footer';
 import Toast from './Toast.js'
@@ -16,7 +14,7 @@ const AddProduct = lazy(() => import(/*webpackChunkName: "ADD" */ './pages/add')
 const Sell = lazy(() => import(/*webpackChunkName: "SELL" */ './pages/sell'))
 const Products = lazy(() => import('./pages/products'))
 const ProductInfo = lazy(() => import('./pages/productInfo'))
-const Scan = lazy(()=> import('./pages/scan'))
+const Scan = lazy(() => import('./pages/scan'))
 const QRCode = lazy(() => import('./pages/qrcode'))
 const AddOwner = lazy(() => import('./pages/addOwner'))
 
@@ -24,7 +22,7 @@ const AddOwner = lazy(() => import('./pages/addOwner'))
 function Routes() {
 
   return (
-      <>
+    <>
       <Router>
         <Route component={Header} />
         <Switch>
@@ -34,81 +32,17 @@ function Routes() {
           <Route path='/login' component={Login} />
           <Route path='/buy' exact component={BuyProduct} />
           <Route path='/productinfo/:id' component={ProductInfo} />
-          <ProtectedOwnerRoute path='/add'>
-            <AddProduct />
-          </ProtectedOwnerRoute>
-          <ProtectedRoute path='/sell'>
-            <Sell />
-          </ProtectedRoute>
-          <ProtectedRoute path='/products'>
-            <Products />
-          </ProtectedRoute>
-          <ProtectedRoute path='/qrcode'>
-            <QRCode />
-          </ProtectedRoute>
-          <ProtectedRoute path='/addowner'>
-            <AddOwner />
-          </ProtectedRoute>
+          <Route path='/add' component={AddProduct} />
+          <Route path='/sell' component={Sell} />
+          <Route path='/products' component={Products} />
+          <Route path='/qrcode' component={QRCode} />
+          <Route path='/addowner' component={AddOwner} />
         </Switch>
         <Route component={Footer} />
       </Router>
       <Toast />
-      </>
+    </>
   );
 }
-
-function ProtectedRoute(comp) {
-
-  const { children, ...rest } = comp
-  const login = useRecoilValue(ll)
-
-  return (
-    <Route
-      {...rest}
-      render={
-        (
-          { location }
-        ) =>
-          login
-            ? children
-            : <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-      }
-    />
-  )
-
-}
-
-function ProtectedOwnerRoute(comp) {
-
-  const { children, ...rest } = comp
-  const login = useRecoilValue(ll);
-  const type = useRecoilValue(ti);
-
-  return (
-    <Route
-      {...rest}
-      render={
-        (
-          { location }
-        ) =>
-          (login && type==='owner')
-            ? children
-            : <Redirect
-              to={{
-                pathname: "/login",
-                state: { from: location }
-              }}
-            />
-      }
-    />
-  )
-
-}
-
 
 export default Routes;
---
+++ app/src/store/abi.js
@@ -0,0 +1,422 @@
+export default {
+    abi: [
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "previousOwner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "internalType": "address",
+                    "name": "newOwner",
+                    "type": "address"
+                }
+            ],
+            "name": "OwnershipTransferred",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "productOwner",
+                    "type": "address"
+                }
+            ],
+            "name": "productAdded",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "buyerAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "productPurchased",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "sellerAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "productSold",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "name": "sellerAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "sellerRegistered",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "sellerAddress",
+                    "internalType": "address",
+                    "type": "address"
+                }
+            ],
+            "name": "sellerReported",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [
+                {
+                    "indexed": false,
+                    "internalType": "address",
+                    "name": "sellerAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "sellerUnblocked",
+            "type": "event"
+        },
+        {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+            "constant": true
+        },
+        {
+            "inputs": [],
+            "name": "renounceOwnership",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "newOwner",
+                    "type": "address"
+                }
+            ],
+            "name": "transferOwnership",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "_sellerAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "unblockSeller",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_details",
+                    "type": "string"
+                }
+            ],
+            "name": "registerSeller",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "status",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_productId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "registerReport",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_secretId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "buyProduct",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getAllProducts",
+            "outputs": [
+                {
+                    "components": [
+                        {
+                            "internalType": "uint256",
+                            "name": "productId",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "price",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "string",
+                            "name": "name",
+                            "type": "string"
+                        },
+                        {
+                            "internalType": "bool",
+                            "name": "isSold",
+                            "type": "bool"
+                        }
+                    ],
+                    "internalType": "struct Counterfeit.Product[]",
+                    "name": "",
+                    "type": "tuple[]"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+            "constant": true
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_productId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "address",
+                    "name": "_buyerAddress",
+                    "type": "address"
+                }
+            ],
+            "name": "sellProduct",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_productId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_secretId",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "_price",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_name",
+                    "type": "string"
+                }
+            ],
+            "name": "addProduct",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_productId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "productSeller",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "details",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+            "constant": true
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_productId",
+                    "type": "uint256"
+                }
+            ],
+            "name": "productDetails",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "price",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "bool",
+                    "name": "isSold",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+            "constant": true
+        },
+        {
+            "inputs": [],
+            "name": "productLength",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_productArrayLength",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+            "constant": true
+        },
+        {
+            "inputs": [],
+            "name": "sellersLength",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "_sellerArrayLength",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function",
+            "constant": true
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "string",
+                    "name": "_name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "_details",
+                    "type": "string"
+                }
+            ],
+            "name": "registerOwnerAsSeller",
+            "outputs": [
+                {
+                    "internalType": "string",
+                    "name": "status",
+                    "type": "string"
+                }
+            ],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        }
+    ]
+}
